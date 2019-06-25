24c17f6ea42418b5b726c8a2d322a5c2a5635309
vmejazz <vmejazz@gmail.com>
2019-06-25 16:46:19 +0300

    Код из 4ого задания

--- js/main.js
+++ js/main.js
@@ -1,21 +1,28 @@
 'use strict';
 
-var PIN_WIDTH = 50;
-var PIN_HEIGHT = 70;
-var PINS_COUNT = 8;
-
 var mapStatus = document.querySelector('.map');
-// mapStatus.classList.remove('map--faded'); // переводим блок карты в активное состояние
 
 var mapOverlay = document.querySelector('.map__overlay');
-// var mapOverlayWidth = mapOverlay.offsetWidth;
-// var mapOverlayHeight = mapOverlay.offsetHeight;
 
 var mapPinsElement = document.querySelector('.map__pins');
 
 var mapPinTemplate = document.querySelector('#pin')
   .content
   .querySelector('.map__pin');
+
+var userForm = document.querySelector('.ad-form');
+var mapActivator = document.querySelector('.map__pin--main');
+var inputAddress = userForm.querySelector('input[name=address]');
+var formsElement = document.querySelectorAll('form');
+
+var PIN_WIDTH = 50;
+var PIN_HEIGHT = 70;
+var PINS_COUNT = 8;
+var LIMIT_PIN_TOP = 130;
+var LIMIT_PIN_BOTTOM = 630;
+var LIMIT_PIN_LEFT = -(mapActivator.offsetWidth / 2);
+var LIMIT_PIN_RIGHT = mapOverlay.offsetWidth - (mapActivator.offsetWidth / 2);
+
 
 var getRandomArbitrary = function (min, max) {
   return Math.round(Math.random() * (max - min) + min);
@@ -69,8 +76,8 @@ var getPinProperty = function (i) {
       },
     'location':
       {
-        'x': getRandomArbitrary(0, mapOverlay.offsetWidth - (PIN_WIDTH * 2)),
-        'y': getRandomArbitrary(130, 630)
+        'x': getRandomArbitrary(LIMIT_PIN_LEFT, LIMIT_PIN_RIGHT),
+        'y': getRandomArbitrary(LIMIT_PIN_TOP, LIMIT_PIN_BOTTOM)
       }
   };
   return pinProperty;
@@ -109,10 +116,6 @@ for (var k = 0; k < arrayPins.length; k++) {
 // module4-task1  -------------------------------------------------------------
 
 
-var userForm = document.querySelector('.ad-form');
-var mapActivator = document.querySelector('.map__pin--main');
-var inputAddress = userForm.querySelector('input[name=address]');
-var formsElement = document.querySelectorAll('form');
 var coordinatePinStart = {
   x: Math.round((mapOverlay.offsetWidth / 2) + (mapActivator.offsetWidth / 2)),
   y: Math.round((mapOverlay.offsetHeight / 2) + (mapActivator.offsetHeight / 2))
@@ -121,7 +124,6 @@ var coordinatePinStart = {
 var getCoordinatePin = function (element) {
   var x = Math.round(element.offsetLeft + mapActivator.offsetWidth / 2);
   var y = Math.round(element.getBoundingClientRect().top);
-
   return (x + ',' + y);
 };
 
@@ -133,14 +135,16 @@ var changeStateElementsForm = function (toggle) {
   }
 };
 
-mapActivator.addEventListener('click', function () {
+var getMapActiveStatus = function () {
   mapStatus.classList.remove('map--faded');
-  mapActivator.removeEventListener('click', function () {});
+  mapActivator.removeEventListener('click', getMapActiveStatus);
   changeStateElementsForm(false);
   userForm.classList.remove('ad-form--disabled');
   mapPinsElement.appendChild(fragment);
   inputAddress.value = getCoordinatePin(mapActivator);
-});
+};
+
+mapActivator.addEventListener('click', getMapActiveStatus);
 
 changeStateElementsForm(true);
 inputAddress.value = coordinatePinStart.x + ',' + coordinatePinStart.y;
@@ -255,5 +259,4 @@ var onPinDown = function (evt) {
   document.addEventListener('mouseup', onPinUpOnMap);
 };
 
-document.addEventListener('mousedown', onPinDown, true);
-
+mapActivator.addEventListener('mousedown', onPinDown, true);
